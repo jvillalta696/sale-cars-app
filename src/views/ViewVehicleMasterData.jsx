1803badf97b1ddab1d62e880d4ef4418a555abca
jvillalta696 <jvillalta696@gmail.com>
2025-01-15 21:13:15 -0600

    feacture:change vehicule data

--- src/views/ViewVehicleMasterData.jsx
+++ src/views/ViewVehicleMasterData.jsx
@@ -1,22 +1,42 @@
-import { useEffect, useState } from 'react';
-import mockVehiculos from '../mockData/mockVehiculos.json';
+import React, { useEffect, useState } from 'react';
 import VehiculoForm from '../components/VehiculoForm';
 import M from 'materialize-css';
+import { useAuth } from '../context/AuthContext';
+import {
+  getVehiculeByVIN,
+  getVehiculesList,
+} from '../services/vehicule.service';
+import { useClient } from '../context/ClientContext';
 
 const ViewVehicleMasterData = () => {
   const [searchVin, setSearchVin] = useState('');
+  const { apiConfig, currentCompany } = useAuth();
   const [selectedVehicles, setSelectedVehicles] = useState([]);
   const [selectedVehicle, setSelectedVehicle] = useState(null);
+  const { loading } = useClient();
 
-  const handleSearchVehicle = () => {
-    const vehicles = mockVehiculos.filter(v => v.VIN.includes(searchVin));
+  const handleSearchVehicle = async () => {
+    const vehicles = await getVehiculesList(
+      apiConfig,
+      currentCompany.code,
+      searchVin
+    );
     setSelectedVehicles(vehicles);
     setSelectedVehicle(null);
   };
 
-  const handleSelectVehicle = (vehicle) => {
-    setSelectedVehicles([]);
-    setSelectedVehicle(vehicle);
+  const handleSelectVehicle = async (VIN) => {
+    try {
+      const vehicleData = await getVehiculeByVIN(
+        apiConfig,
+        currentCompany.code,
+        VIN
+      );
+      setSelectedVehicles([]);
+      setSelectedVehicle(vehicleData);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   const handleKeyPress = (event) => {
@@ -29,37 +49,51 @@ const ViewVehicleMasterData = () => {
     M.updateTextFields();
   }, [selectedVehicle]);
 
+  useEffect(() => {
+    if (loading) {
+      setSelectedVehicles([]);
+      setSelectedVehicle(null);
+      setSearchVin('');
+    }
+  }, [loading]);
+
   return (
     <div className="container">
       <div className="card">
         <div className="card-content">
-          <span className="card-title">Consulta de Vehiculos por VIN</span>          
+          <span className="card-title">Consulta de Vehiculos por VIN</span>
           <div className="input-field">
-            <input 
-              type="text" 
-              id="searchVin" 
-              placeholder="Digite el VIN a buscar" 
+            <input
+              type="text"
+              id="searchVin"
+              placeholder="Digite el VIN a buscar"
               value={searchVin}
               onChange={(e) => setSearchVin(e.target.value)}
               onKeyPress={handleKeyPress}
             />
             <label htmlFor="searchVin">Buscar vehículo</label>
           </div>
-          <button onClick={handleSearchVehicle} className="btn">Buscar</button>
+          <button onClick={handleSearchVehicle} className="btn">
+            Buscar
+          </button>
           {selectedVehicles.length > 0 && (
             <>
-            <div className="h6">Vehículos</div>
-            <div className="collection">
-              {selectedVehicles.map(vehicle => (
-                  <a href="#!" className="collection-item" key={vehicle.VIN} onClick={() => handleSelectVehicle(vehicle)}>
-                    {vehicle.VIN} - {vehicle.Marca} {vehicle.Estilo} {vehicle.Año}
+              <div className="h6">Vehículos</div>
+              <div className="collection">
+                {selectedVehicles.map((vehicle) => (
+                  <a
+                    href="#!"
+                    className="collection-item"
+                    key={vehicle.VIN}
+                    onClick={() => handleSelectVehicle(vehicle.VIN)}
+                  >
+                    {vehicle.VIN} - {vehicle.Marca} {vehicle.Estilo}{' '}
+                    {vehicle.Año}
                   </a>
                 ))}
-            </div>                
-             </>
-          )}           
-            
-          
+              </div>
+            </>
+          )}
         </div>
       </div>
       {selectedVehicle && <VehiculoForm data={selectedVehicle} />}
